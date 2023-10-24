c9340c913a4b7b6978c37560b64dd9446ba844c9
Roar Emaus <roar@emaus.no>
2023-10-24 20:29:22 +0200

    changed how to read locked scores

--- api.py
+++ api.py
@@ -5,40 +5,48 @@ from fastapi.middleware.cors import CORSMiddleware
 from pathlib import Path
 import redis
 
+### TYPES
 class Game(BaseModel):
     name: str
     scores: Dict[str, int]
 
 
-class Games(BaseModel):
+class BjorliGame(BaseModel):
     date: str
+    locked: bool
     games: List[Game]
     players: List[str]
 
 
-def start_up():
-    data_folder = Path('/workspaces/bjorligame/data')
-    games = []
-    for game_folder in data_folder.iterdir():
-        if game_folder.is_dir():
-            with open(game_folder/'poeng.csv') as score_file:
-                game_names = score_file.readline().strip().split(',')[1:]
-                date = game_folder.name.replace('_','-')
-                
-                players = {}
-                for player in score_file.readlines():
-                    player_scores = player.strip().split(',')
-                    players[player_scores[0]] = list(map(int ,player_scores[1:]))
-                this_bjorligame = []
-                for game_index, game_name in enumerate(game_names):
-                    this_bjorligame.append(
-                        Game(name=game_name, scores={pn: s[game_index] for pn, s in players.items()})
-                    )
-            games.append(Games(date=date, games=this_bjorligame, players=list(players.keys())))
-    return games
+### UTIL FUNCTIONS
+
+def get_locked_games(data_path: Path) -> List[BjorliGame]:
+    all_dates = []
+    for file_path in get_all_game_paths(data_path):
+        all_dates.append(csv_to_bjorligame(file_path))
+    return all_dates
 
 
-# games = start_up()
+def get_all_game_paths(data_path: Path) -> List[Path]:
+    return [filename for filename in data_path.iterdir() if filename.match("*.csv")]
+
+
+def csv_to_bjorligame(file_path: Path) -> BjorliGame:
+    with open(file_path) as score_file:
+        game_names = score_file.readline().strip().split(',')[1:]
+        date = file_path.name.strip(".csv").replace('_','-')
+                        
+        players = {}
+        for player in score_file.readlines():
+            player_scores = player.strip().split(',')
+            players[player_scores[0]] = list(map(int ,player_scores[1:]))
+        games = []
+        for game_index, game_name in enumerate(game_names):
+            games.append(
+                Game(name=game_name, scores={pn: s[game_index] for pn, s in players.items()})
+            )
+    return BjorliGame(date=date, locked=True, games=games, players=list(players.keys()))
+
 
 app = FastAPI()
 origins = [
@@ -53,53 +61,27 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# @app.get("/date/")
-# async def root() -> Games:
-#     return Games(
-#         date='',
-#         games=[Game(name='No name', scores={'No name': 0})], players=['No name'])
-
-# @app.get("/date/{date}")
-# async def root(date: str) -> Games:
-#     print(f"Called /date/{date}")
-#     for bjorligame in games:
-#         if date == bjorligame.date:
-#             return bjorligame
-#     return Games(
-#         date='',
-#         games=[Game(name='No name', scores={'No name': 0})], players=['No name'])
-
-
-# @app.get("/dates")
-# async def get_dates() -> List[str]:
-#     print("Called /dates: ", [g.date for g in games])
-#     return sorted([g.date for g in games])
-
-# @app.post("/game")
-# async def add_game(game: Games) -> Games:
-#     print("Called /game", game)
-#     # games.append(game)
-#     return game
-
-
 redis_client = redis.Redis(host='localhost', port=6379, db=0)
 
 @app.get("/date/")
-async def root() -> Games:
-    return Games(
+async def root() -> BjorliGame:
+    return BjorliGame(
         date='',
+        locked=False,
         games=[Game(name='No name', scores={'No name': 0})], players=['No name'])
 
 @app.get("/date/{date}")
-async def root(date: str) -> Games:
+async def root(date: str) -> BjorliGame:
     print(f"Called /date/{date}")
     games = redis_client.lrange(date, 0, -1)
+    print(games)
     if not games:
-        return Games(
+        return BjorliGame(
             date='',
+            locked=False,
             games=[Game(name='No name', scores={'No name': 0})], players=['No name'])
     latest_game = games[-1].decode('utf-8')
-    return Games.model_validate_json(latest_game)
+    return BjorliGame.model_validate_json(latest_game)
 
 
 @app.get("/dates")
@@ -110,7 +92,7 @@ async def get_dates() -> List[str]:
 
 
 @app.post("/game")
-async def add_game(game: Games) -> Games:
+async def add_game(game: BjorliGame) -> BjorliGame:
     print("Called /game", game)
     timestamp = redis_client.time()[0]
     game_data = game.model_dump_json()
@@ -120,3 +102,33 @@ async def add_game(game: Games) -> Games:
     redis_client.set('latest_date', game.date)
     redis_client.save()
     return game
+
+
+def add_locked_game_to_redis(game: BjorliGame):
+    game_data = game.model_dump_json()
+    add_game_to_redis(game.date, game_data)
+    redis_client.xadd(game.date + '_locked', {game_data: 'locked'})
+
+
+def add_game_to_redis(date, game_data):
+    redis_client.rpush(date, game_data)
+    redis_client.lpush('dates', date)
+    
+
+if __name__ == "__main__":
+    import argparse
+    import uvicorn
+    # Initialize the argument parser
+    parser = argparse.ArgumentParser(description="FastAPI App with Command-Line Arguments")
+
+    # Add the arguments you need
+    parser.add_argument("--locked", default=None, help="The path of the locked scores")
+
+    # Parse the command-line arguments
+    args = parser.parse_args()
+    locked_path = Path(args.locked)
+    all_locked_games = get_locked_games(locked_path)
+    for game in all_locked_games:
+        add_locked_game_to_redis(game)
+
+    uvicorn.run('api:app', host='0.0.0.0', port=8000, reload=True)
--- run_uvicorn_dev.sh
+++ run_uvicorn_dev.sh
@@ -1,1 +1,1 @@
-podman run -it --rm --network=host --workdir=/bjorligames -v $(pwd):/bjorligames localhost/react-dev:latest uvicorn --reload api:app
+podman run -it --rm --network=host --workdir=/bjorligames -v $(pwd):/bjorligames localhost/react-dev:latest python api.py --locked=/bjorligames/data/locked_scores
