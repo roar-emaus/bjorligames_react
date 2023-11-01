04a2bb64a06a761f3c2d98e82e88594d80c51a1b
Roar Emaus <roar@emaus.no>
2023-11-01 21:24:41 +0100

    endret tittel på sida og bruker gunicorn istedenfor uvicorn til å hoste

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ FROM docker.io/library/alpine:latest as builder
 RUN apk add --no-cache git npm python3 py3-pip
 
 # Install Python packages
-RUN pip3 install uvicorn fastapi pydantic
+RUN pip3 install uvicorn[standard] fastapi pydantic gunicorn
 
 # Clone the repository and set the working directory
 RUN git clone https://github.com/roar-emaus/bjorligames_react.git
@@ -22,7 +22,7 @@ FROM docker.io/library/alpine:latest
 RUN apk add --no-cache python3 py3-pip
 
 # Install Python packages
-RUN pip3 install uvicorn fastapi pydantic
+RUN pip3 install uvicorn[standard] fastapi pydantic gunicorn
 
 # Copy necessary files from the build stage
 COPY --from=builder /bjorligames_react/project/dist /bjorligames_react/project/dist
@@ -31,4 +31,6 @@ COPY --from=builder /bjorligames_react/api.py /bjorligames_react/api.py
 
 # Set the working directory and default command
 WORKDIR /bjorligames_react
-CMD ["python3", "api.py", "--data_path=/bjorligames_react/data/"]
+ENV DATA_PATH=/bjorligames_react/data/
+
+CMD ["gunicorn", "-w", "4", "-k", "uvicorn.workers.UvicornWorker", "api:app", "--bind", "0.0.0.0:80"]
--- build_runtime.sh
+++ build_runtime.sh
@@ -5,7 +5,7 @@ set -e
 # Build stage using Alpine
 bc_build=$(buildah from docker.io/library/alpine:latest)
 buildah run $bc_build apk add --no-cache git npm python3 py3-pip
-buildah run $bc_build pip3 install uvicorn fastapi pydantic
+buildah run $bc_build pip3 install uvicorn[standard] fastapi pydantic gunicorn
 
 buildah run $bc_build git clone https://github.com/roar-emaus/bjorligames_react.git
 buildah config --workingdir=/bjorligames_react/project $bc_build
@@ -15,14 +15,16 @@ buildah run $bc_build npm run build
 # Final stage using Alpine
 bc=$(buildah from docker.io/library/alpine:latest)
 buildah run $bc apk add --no-cache python3 py3-pip
-buildah run $bc pip3 install uvicorn fastapi pydantic
+buildah run $bc pip3 install uvicorn[standard] fastapi pydantic gunicorn
 
 buildah copy --from=$bc_build $bc /bjorligames_react/project/dist /bjorligames_react/project/dist
 buildah copy --from=$bc_build $bc /bjorligames_react/data /bjorligames_react/data
 buildah copy --from=$bc_build $bc /bjorligames_react/api.py /bjorligames_react/api.py
 
 buildah config --workingdir=/bjorligames_react/ $bc
-buildah config --cmd "python3 api.py --data_path=/bjorligames_react/data/" $bc
+buildah config --env DATA_PATH=/bjorligames_react/data/ $bc
+
+buildah config --cmd "gunicorn -w 4 -k uvicorn.workers.UvicornWorker api:app --bind 0.0.0.0:80" $bc
 
 buildah commit $bc localhost/bjorligames:latest
 buildah rm $bc $bc_build
